b6642c1fc8374c387b51a029a9ca955776d73a5d
Tom G Varik <tgvarik@me.com>
2014-10-19 13:24:50 -0700

    Initial Commit

---
+++ colorfile/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  colorfile
+//
+//  Created by Tom G Varik on 10/19/14.
+//  Copyright (c) 2014 Tom G Varik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
